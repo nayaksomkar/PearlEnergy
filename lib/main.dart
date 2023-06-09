b17bf6582425c702df3244e86403189e6afe4a98
nerd <thenerdnyk@gmail.com>
2023-06-10 02:24:38 +0530

    modified:   lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text('Flutte52432654r is fun'),
+        ),
+        body: Container(
+          margin: const EdgeInsets.all(10),
+          child: const Text('e65353fhyuefg'),
+        ),
+      ),
+    );
+  }
+}
