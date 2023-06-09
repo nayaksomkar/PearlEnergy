420c71b99c95f98ac073b78d0a91bfd115a7decd
nerd <thenerdnyk@gmail.com>
2023-06-10 01:15:53 +0530

    new file

---
+++ lib/widgets/navigation/navigation_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class name extends StatelessWidget {
+  const name({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
