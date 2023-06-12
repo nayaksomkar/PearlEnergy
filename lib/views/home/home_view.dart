9f8fcd9cb80b3ead23728cecdf1a2ca642daa3b3
nerd <thenerdnyk@gmail.com>
2023-06-12 12:52:25 +0530

    modified:   lib/views/home/home_view.dart modified:   lib/widgets/navigation_bar/navigation_bar.dart

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:pearl_energy/widgets/navigation_bar/navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: <Widget>[
+            NavBar(),
+          ],
+        ));
   }
 }
--- lib/widgets/navigation_bar/navigation_bar.dart
+++ lib/widgets/navigation_bar/navigation_bar.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
 
-class NavigationBar extends StatelessWidget {
-  const NavigationBar({super.key});
+class NavBar extends StatelessWidget {
+  const NavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       height: 100,
       child: Row(
-        children: [
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
           SizedBox(
             height: 80,
             width: 150,
             child: Image.asset('assets.logo.jpg'),
           ),
-
           const Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-              _NavBarItem(title: 'Episodes',),
-              SizedBox(width: 60,),
+              _NavBarItem(
+                title: 'Episodes',
+              ),
+              SizedBox(
+                width: 60,
+              ),
               _NavBarItem(title: 'About'),
             ],
           )
