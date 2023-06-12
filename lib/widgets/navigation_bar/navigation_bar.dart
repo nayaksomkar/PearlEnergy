a4af125667b3ad342cc60d195c658d89dcd750b4
nerd <thenerdnyk@gmail.com>
2023-06-12 13:22:00 +0530

    replaced Container with SizedBox

--- lib/widgets/navigation_bar/navigation_bar.dart
+++ lib/widgets/navigation_bar/navigation_bar.dart
@@ -5,7 +5,7 @@ class NavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: 100,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
