13466a06a6ebc925ba94bd38821d283fc7b02c28
nerd <thenerdnyk@gmail.com>
2023-06-15 12:23:56 +0530

    modified:   lib/pages/sample_page.dart

---
+++ lib/pages/sample_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {},
+          child: const Text('Go Back'),
+        ),
+      ),
+    );
+  }
+}
