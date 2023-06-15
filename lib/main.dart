12ebebc138634da81e618eb851104657b74c8d39
nerd <thenerdnyk@gmail.com>
2023-06-16 02:21:30 +0530

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,49 +12,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // materialApp with debugbanner false
-    return MaterialApp(
-      // theme of the app
-      theme: ThemeData(primarySwatch: Colors.amber),
-      debugShowCheckedModeBanner: false,
-      // scaffold with app
-      home: Scaffold(
-        // appbat sets the title of the app
-        // Decoratedbox which takes the
-        // decoration and child property
-        body: DecoratedBox(
-          // BoxDecoration takes the image
-          decoration: const BoxDecoration(
-            // Image set to background of the body
-            image: DecorationImage(
-                image: AssetImage('assets/images/BackgroundImage.jpg'),
-                fit: BoxFit.fill),
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/BackgroundImage.jpg"),
+            fit: BoxFit.fill,
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            // Logo will be shown above the background image
-            children: [
-              const ImageIcon(
-                AssetImage('assets/images/LogoTransparent.png'),
-                size: 420,
-                color: Colors.amber,
-              ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amber,
-                    foregroundColor: Colors.amber,
-                  ),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return const HomePage(title: 'SecondPage');
-                    }));
-                  },
-                  child: const Text(
-                    'login to continue',
-                  ))
-            ],
-          ),
+        ),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Add other widgets here
+          ],
         ),
       ),
     );
