77f9131c084e472ed7a996d251b136a929f824c8
nerd <thenerdnyk@gmail.com>
2023-06-15 21:44:23 +0530

    modified:   lib/pages/welcome_page.dart

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -8,13 +8,16 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       body: Center(
         child: ElevatedButton(
           onPressed: () {
             Navigator.push(context, MaterialPageRoute(builder: (context) {
               return const HomePage(title: 'HomePage');
-            }));
+            }
+            ));
           },
+          
           style: ButtonStyle(
             backgroundColor: MaterialStateProperty.all(
                 const Color.fromARGB(255, 179, 172, 42)),
