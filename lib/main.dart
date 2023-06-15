105cc45b70a6483082c3e4719726747d3f7a3d01
nerd <thenerdnyk@gmail.com>
2023-06-15 22:03:25 +0530

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,3 @@ import 'pages/welcome_page.dart';
 void main() {
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          scaffoldBackgroundColor: const Color.fromARGB(255, 0, 0, 0)),
-      home: const WelcomePage(title: 'Pearl Energy'),
-    );
-  }
-}
