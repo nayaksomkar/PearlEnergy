220b021e4022b7fcf071a7356d135a99805bb479
nerd <thenerdnyk@gmail.com>
2023-06-16 23:32:14 +0530

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/BackgroundImage.jpg"),
-            fit: BoxFit.fill,
+    // materialApp with debugbanner false
+    return MaterialApp(
+      // theme of the app
+      theme: ThemeData(primarySwatch: Colors.green),
+      debugShowCheckedModeBanner: false,
+      // scaffold with app
+      home: Scaffold(
+        // appbat sets the title of the app
+
+        // Decoratedbox which takes the
+        // decoration and child property
+        body: Container(
+          // BoxDecoration takes the image
+          decoration: const BoxDecoration(
+            // Image set to background of the body
+            image: DecorationImage(
+                image: AssetImage("assets/images/BackgroundImage.jpg"),
+                fit: BoxFit.fill),
           ),
-        ),
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Add other widgets here
-          ],
+          child: const Column(
+              // flutter logo that will shown
+              // above the background image
+              children: [
+                FlutterLogo(
+                  size: 200 * 2,
+                ),
+                ElevatedButton(
+                    onPressed: null, child: Text('login to continue'))
+              ]),
         ),
       ),
     );
