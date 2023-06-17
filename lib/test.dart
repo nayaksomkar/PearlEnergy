e79459a3325f73ada89d3840116225fcea060fca
nerd <thenerdnyk@gmail.com>
2023-06-17 20:38:18 +0530

    modified:   lib/main.dart new file:   lib/test.dart modified:   test/widget_test.dart

---
+++ lib/test.dart
@@ -0,0 +1,48 @@
+Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              // flutter logo that will shown
+              // above the background image
+              children: [
+                const Image(
+                  image: AssetImage(
+                    'assets/images/LogoTransparent.png',
+                  ),
+                  fit: BoxFit.cover,
+                ),
+                ElevatedButton(
+                  style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(Colors.amber),
+                      foregroundColor: MaterialStatePropertyAll(Colors.black)),
+                  child: const Text(
+                    'User Login',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return const HomePage(title: 'SecondPage');
+                    }));
+                  },
+                ),
+                const Text(''),
+                const ElevatedButton(
+                  onPressed: null,
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(Colors.amber),
+                      foregroundColor: MaterialStatePropertyAll(Colors.black)),
+                  child: Text(
+                    'Admin Login',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    textStyle: const TextStyle(
+                        fontSize: 10, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Request for a connection'),
+                ),
+              ]),
