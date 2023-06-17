ce884f218b162cc1c5e75e556799cc2cc1962bcf
nerd <thenerdnyk@gmail.com>
2023-06-18 00:37:23 +0530

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Naviation Demo',
+      title: 'PearlEnergy',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.black,
         primarySwatch: Colors.amber,
