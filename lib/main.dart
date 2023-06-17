f5a33a446a35d8e276c05a9ab29837edfff34830
nerd <thenerdnyk@gmail.com>
2023-06-17 23:58:57 +0530

    AppBar removed

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,6 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
       body: Center(
         child: TextButton(
           onPressed: () {},
