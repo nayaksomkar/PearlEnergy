43fb82841d0de30e7a42f2c73f914ce9605cfad9
nerd <thenerdnyk@gmail.com>
2023-06-18 00:54:20 +0530

    modified:   lib/pages/login/userlogin.dart

--- lib/pages/login/adminlogin.dart
+++ lib/pages/login/adminlogin.dart
@@ -7,6 +7,7 @@ class AdminLoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(title),
       ),
       body: Center(
--- lib/pages/login/userlogin.dart
+++ lib/pages/login/userlogin.dart
@@ -7,6 +7,7 @@ class UserLoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(title),
       ),
       body: Center(
