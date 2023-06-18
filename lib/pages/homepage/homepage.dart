31a4e66318dddd6518c727c0ebeee75e5dccd687
nerd <thenerdnyk@gmail.com>
2023-06-18 12:42:01 +0530

    modified:   lib/pages/homepage/homepage.dart modified:   pubspec.lock modified:   pubspec.yaml

--- lib/pages/homepage/homepage.dart
+++ lib/pages/homepage/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sample/main.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key, required this.title});
@@ -20,6 +21,29 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            ImageSlideshow(
+              width: double.infinity,
+              height: 200,
+              initialPage: 0,
+              indicatorColor: Colors.blue,
+              indicatorBackgroundColor: Colors.grey,
+              autoPlayInterval: 3,
+              isLoop: true,
+              children: [
+                Image.asset(
+                  'images/sample_image_1.jpg',
+                  fit: BoxFit.cover,
+                ),
+                Image.asset(
+                  'images/sample_image_2.jpg',
+                  fit: BoxFit.cover,
+                ),
+                Image.asset(
+                  'images/sample_image_3.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ],
+            ),
             ElevatedButton(
               style: const ButtonStyle(
                   backgroundColor: MaterialStatePropertyAll(Colors.amber),
