e17a01f986716273fa8028813c0c70d5268df5e5
nerd <thenerdnyk@gmail.com>
2023-06-21 23:20:03 +0530

    FirebaseDatabase

--- lib/FirebaseDatabase/firebasedb.dart
+++ lib/FirebaseDatabase/firebasedb.dart
@@ -1,9 +1,3 @@
 import 'package:firebase_database/firebase_database.dart';
 
 
-DatabaseReference starCountRef =
-        FirebaseDatabase.instance.ref('posts/$postId/starCount');
-starCountRef.onValue.listen((DatabaseEvent event) {
-    final data = event.snapshot.value;
-    updateStarCount(data);
-});
