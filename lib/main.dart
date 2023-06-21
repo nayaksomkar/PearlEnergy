0dfafb0b038410fd07cc1754d136b73473a0344a
nerd <thenerdnyk@gmail.com>
2023-06-21 18:56:38 +0530

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,3 @@
 import 'package:flutter/material.dart';
-import 'pages/login/loginoption.dart';
 
-void main() {
-// main method thats
-// run the MyApp
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PearlEnergy',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Colors.black,
-        primarySwatch: Colors.amber,
-      ),
-      home: const LoginOption(title: 'LoginOption'),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
+db = FirebaseFirestore.instance;
