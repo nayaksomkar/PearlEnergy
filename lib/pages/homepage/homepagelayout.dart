dc19ddedecff6b3558637d823e5d20f64a263a82
nerd <thenerdnyk@gmail.com>
2023-07-13 22:02:21 +0530

    modified:   lib/pages/homepage/homepagelayout.dart

--- lib/pages/homepage/homepagelayout.dart
+++ lib/pages/homepage/homepagelayout.dart
@@ -11,241 +11,244 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Image(
-                  image: AssetImage(
-                    'assets/images/IconTransparent.png',
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Image(
+                    image: AssetImage(
+                      'assets/images/IconTransparent.png',
+                    ),
+                    height: 200,
+                    width: 200,
                   ),
-                  height: 200,
-                  width: 200,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                // ignore: prefer_const_constructors
-                Text(
-                  'Payments and Bills',
+                  const SizedBox(
+                    height: 10,
+                  ),
                   // ignore: prefer_const_constructors
-                  style: TextStyle(
-                    color: Colors.amberAccent,
-                    decoration: TextDecoration.underline,
-                    fontSize: 15,
+                  Text(
+                    'Payments and Bills',
+                    // ignore: prefer_const_constructors
+                    style: TextStyle(
+                      color: Colors.amberAccent,
+                      decoration: TextDecoration.underline,
+                      fontSize: 15,
+                    ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            onPressed: () {
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) {
-                                return const PaymentHistoryPage();
-                              }));
-                            },
-                            icon: const Icon(
-                              Icons.payments_rounded,
-                              size: 40,
-                            )),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'Payment History',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white.withOpacity(0.7)),
-                        )
-                      ],
-                    ),
-                    const SizedBox(width: 100),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            onPressed: () {
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) {
-                                return const PaymentStatusPage();
-                              }));
-                            },
-                            icon: const Icon(
-                              Icons.price_check_rounded,
-                              size: 40,
-                            )),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'Payment Status & Bill',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white.withOpacity(0.7)),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                // ignore: prefer_const_constructors
-                Text(
-                  'Ussage and Consumption',
-                  textAlign: TextAlign.center,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return const PaymentHistoryPage();
+                                }));
+                              },
+                              icon: const Icon(
+                                Icons.payments_rounded,
+                                size: 40,
+                              )),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'Payment History',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white.withOpacity(0.7)),
+                          )
+                        ],
+                      ),
+                      const SizedBox(width: 100),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return const PaymentStatusPage();
+                                }));
+                              },
+                              icon: const Icon(
+                                Icons.price_check_rounded,
+                                size: 40,
+                              )),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'Payment Status & Bill',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white.withOpacity(0.7)),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
                   // ignore: prefer_const_constructors
-                  style: TextStyle(
-                    color: Colors.amberAccent,
-                    decoration: TextDecoration.underline,
-                    fontSize: 15,
+                  Text(
+                    'Ussage and Consumption',
+                    textAlign: TextAlign.center,
+                    // ignore: prefer_const_constructors
+                    style: TextStyle(
+                      color: Colors.amberAccent,
+                      decoration: TextDecoration.underline,
+                      fontSize: 15,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            onPressed: () {
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) {
-                                return const PaymentHistoryPage();
-                              }));
-                            },
-                            icon: const Icon(
-                              Icons.history_edu_rounded,
-                              size: 40,
-                            )),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'Ussage History',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white.withOpacity(0.7)),
-                        )
-                      ],
-                    ),
-                    const SizedBox(width: 100),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.stacked_bar_chart_rounded,
-                              size: 40,
-                            )),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'Current Ussage',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white.withOpacity(0.7)),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                // ignore: prefer_const_constructors
-                Text(
-                  'Manage Account',
-                  textAlign: TextAlign.center,
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return const PaymentHistoryPage();
+                                }));
+                              },
+                              icon: const Icon(
+                                Icons.history_edu_rounded,
+                                size: 40,
+                              )),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'Ussage History',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white.withOpacity(0.7)),
+                          )
+                        ],
+                      ),
+                      const SizedBox(width: 100),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.stacked_bar_chart_rounded,
+                                size: 40,
+                              )),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'Current Ussage',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white.withOpacity(0.7)),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
                   // ignore: prefer_const_constructors
-                  style: TextStyle(
-                    color: Colors.amberAccent,
-                    decoration: TextDecoration.underline,
-                    fontSize: 15,
+                  Text(
+                    'Manage Account',
+                    textAlign: TextAlign.center,
+                    // ignore: prefer_const_constructors
+                    style: TextStyle(
+                      color: Colors.amberAccent,
+                      decoration: TextDecoration.underline,
+                      fontSize: 15,
+                    ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.person_2_rounded,
-                              size: 40,
-                            )),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'Account',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white.withOpacity(0.7)),
-                        )
-                      ],
-                    ),
-                    const SizedBox(width: 145),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            onPressed: () {
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) {
-                                return const MyApp();
-                              }));
-                            },
-                            icon: const Icon(
-                              Icons.login_rounded,
-                              size: 40,
-                            )),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'Logout',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white.withOpacity(0.7)),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.person_2_rounded,
+                                size: 40,
+                              )),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'Account',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white.withOpacity(0.7)),
+                          )
+                        ],
+                      ),
+                      const SizedBox(width: 145),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return const MyApp();
+                                }));
+                              },
+                              icon: const Icon(
+                                Icons.login_rounded,
+                                size: 40,
+                              )),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'Logout',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white.withOpacity(0.7)),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
