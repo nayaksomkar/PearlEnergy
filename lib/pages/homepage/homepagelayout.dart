3c195633369a95639820af9771dac221549fd293
nerd <thenerdnyk@gmail.com>
2023-09-02 16:11:39 +0530

    modified:   lib/pages/homepage/homepagelayout.dart

--- lib/pages/homepage/homepagelayout.dart
+++ lib/pages/homepage/homepagelayout.dart
@@ -233,66 +233,197 @@ class HomePage extends StatelessWidget {
                         ],
                       ),
                       const SizedBox(width: 165),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            height: 75,
-                            width: 75,
-                            child: IconButton(
-                                onPressed: () {
-                                  showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        title: const Center(
-                                          child: Text(
-                                            'WARNING',
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                        ),
-                                        content: const Text(
-                                            'YOU WILL BE REDIRECTED TO THE LOGIN PAGE.'),
-                                        actions: [
-                                          TextButton(
-                                            onPressed: () {
-                                              Navigator.push(context,
-                                                  MaterialPageRoute(
-                                                      builder: (context) {
-                                                return const MyApp();
-                                              }));
-                                            },
-                                            child: const Text('OK'),
-                                          ),
-                                          TextButton(
-                                            onPressed: () {
-                                              Navigator.pop(context);
-                                            },
-                                            child: const Text('CANCEL'),
-                                          ),
-                                        ],
-                                      );
-                                    },
-                                  );
-                                },
-                                icon: Image.asset(
-                                  'assets/gif/logout.gif',
+                      LayoutBuilder(
+                        builder:
+                            (BuildContext context, BoxConstraints constraints) {
+                          if (constraints.maxWidth > 600) {
+                            // For desktop devices
+                            return Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                SizedBox(
                                   height: 100,
                                   width: 100,
-                                )),
-                          ),
-                          Text(
-                            'Logout',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.white.withOpacity(0.7)),
-                          )
-                        ],
-                      ),
+                                  child: IconButton(
+                                    onPressed: () {
+                                      showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return AlertDialog(
+                                            title: const Center(
+                                              child: Text(
+                                                'WARNING',
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ),
+                                            content: const Text(
+                                                'YOU WILL BE REDIRECTED TO THE LOGIN PAGE.'),
+                                            actions: [
+                                              TextButton(
+                                                onPressed: () {
+                                                  Navigator.push(context,
+                                                      MaterialPageRoute(
+                                                          builder: (context) {
+                                                    return const MyApp();
+                                                  }));
+                                                },
+                                                child: const Text('OK'),
+                                              ),
+                                              TextButton(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: const Text('CANCEL'),
+                                              ),
+                                            ],
+                                          );
+                                        },
+                                      );
+                                    },
+                                    icon: Image.asset(
+                                      'assets/gif/logout.gif',
+                                      height: 100,
+                                      width: 100,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  'Logout',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.white.withOpacity(0.7)),
+                                )
+                              ],
+                            );
+                          } else if (constraints.maxWidth > 400) {
+                            // For tablet devices
+                            return Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  height: 75,
+                                  width: 75,
+                                  child: IconButton(
+                                    onPressed: () {
+                                      showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return AlertDialog(
+                                            title: const Center(
+                                              child: Text(
+                                                'WARNING',
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ),
+                                            content: const Text(
+                                                'YOU WILL BE REDIRECTED TO THE LOGIN PAGE.'),
+                                            actions: [
+                                              TextButton(
+                                                onPressed: () {
+                                                  Navigator.push(context,
+                                                      MaterialPageRoute(
+                                                          builder: (context) {
+                                                    return const MyApp();
+                                                  }));
+                                                },
+                                                child: const Text('OK'),
+                                              ),
+                                              TextButton(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: const Text('CANCEL'),
+                                              ),
+                                            ],
+                                          );
+                                        },
+                                      );
+                                    },
+                                    iconSize: 50,
+                                    icon: Image.asset('assets/gif/logout.gif',
+                                        height: 50, width: 50),
+                                  ),
+                                ),
+                                Text(
+                                  'Logout',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.white.withOpacity(0.7)),
+                                )
+                              ],
+                            );
+                          } else {
+                            // For mobile devices
+                            return Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  height: 50,
+                                  width: 50,
+                                  child: IconButton(
+                                    onPressed: () {
+                                      showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return AlertDialog(
+                                            title: const Center(
+                                              child: Text(
+                                                'WARNING',
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                            ),
+                                            content: const Text(
+                                                'YOU WILL BE REDIRECTED TO THE LOGIN PAGE.'),
+                                            actions: [
+                                              TextButton(
+                                                onPressed: () {
+                                                  Navigator.push(context,
+                                                      MaterialPageRoute(
+                                                          builder: (context) {
+                                                    return const MyApp();
+                                                  }));
+                                                },
+                                                child: const Text('OK'),
+                                              ),
+                                              TextButton(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: const Text('CANCEL'),
+                                              ),
+                                            ],
+                                          );
+                                        },
+                                      );
+                                    },
+                                    iconSize: 30,
+                                    icon: Image.asset('assets/gif/logout.gif',
+                                        height: 30, width: 30),
+                                  ),
+                                ),
+                                Text(
+                                  'Logout',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      color: Colors.white.withOpacity(0.7)),
+                                )
+                              ],
+                            );
+                          }
+                        },
+                      )
                     ],
                   ),
                 ],
